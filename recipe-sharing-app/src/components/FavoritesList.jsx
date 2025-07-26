7857d6409091709a3e21bca048d455d9b088f80d
Besomi22 <sbelllali000@gmail.com>
2025-07-26 16:49:11 +0100

    Implementing User Favorites and Personalized Recipe Recommendations

---
+++ recipe-sharing-app/src/components/FavoritesList.jsx
@@ -0,0 +1,24 @@
+import { use } from "react";
+import { useRecipeStore } from "./recipeStore";
+
+const FavoritesList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+  const favorites = useRecipeStore((state) => state.favorites);
+
+  const favoriteRecipes = recipes.filter((recipe) =>
+    favorites.includes(recipe.id)
+  );
+  return (
+    <div>
+      <h2>My favorites</h2>
+      {favoriteRecipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FavoritesList;
