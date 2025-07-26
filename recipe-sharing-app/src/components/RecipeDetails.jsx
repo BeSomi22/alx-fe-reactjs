a3897bce68bbd2799e759dbb5688e42d24ba7ddc
Besomi22 <sbelllali000@gmail.com>
2025-07-26 16:02:33 +0100

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,22 @@
+import { useParams } from "react-router-dom";
+import { useRecipeStore } from "./recipeStore";
+import EditRecipeForm from "./EditRecipeForm";
+import DeleteRecipeButton from "./DeleteRecipeButton";
+
+const RecipeDetails = () => {
+  const { id } = useParams();
+  const recipe = useRecipeStore((state) =>
+    state.recipe.find((r) => r.id == Number(id))
+  );
+
+  if (!recipe) return <p>Recipe not Found.</p>;
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <EditRecipeForm />
+      <DeleteRecipeButton />
+    </div>
+  );
+};
+export default RecipeDetails;
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RecipeDetails from "./components/RecipeDetails.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
