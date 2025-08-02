7519e1adb9c19b7e180c04b11cb62e16dcefcdf9
Besomi22 <sbelllali000@gmail.com>
2025-08-02 21:04:45 +0100

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,16 +2,27 @@ import axios from "axios";
 
 const API_URL = "https://api.github.com/users/";
 
-export const fetchUserData = async (username) => {
+export const fetchUserData = async ({ username, location, minRepos }) => {
   try {
-    const response = await axios.get(`${API_URL}${username}`, {
-      headers: {
-        Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY || ""}`,
-      },
-    });
-    return response.data;
+    //Build query string dynamically
+    let query = "";
+    if (username) query += `${username} in:login`;
+    if (location) query += `location:${location}`;
+    if (miniRepos) query += `repos:>=${minRepos}`;
+
+    const response = await axios.get(
+      `${API_URL}?q=${encodeURIComponent(query)}`,
+      {
+        headers: {
+          Authorization: `token ${
+            import.meta.env.VITE_APP_GITHUB_API_KEY || ""
+          }`,
+        },
+      }
+    );
+    return response.data; //This contains "items" (list of users)
   } catch (error) {
-    console.error("Error fetching GitHub user:", error);
+    console.error("Error searching GitHub user:", error);
     throw error;
   }
 };
